6d7a66b99746970ffc07d84c75bd045c5be45ea3
NYOBE Hubert <hnyobe0@gmail.com>
2023-01-09 13:50:35 +0100

    correction du servicepoint

---
+++ controller/session.js
@@ -0,0 +1,55 @@
+const { Session } = require("../models/sessions");
+const { User } = require("../models/user");
+const { Spaccount } = require("../models/spaccount");
+const { ServicePoint } = require("../models/servicepoint");
+
+const GetSpByName = async (name) => {
+  const getSpAccount = await ServicePoint.findOne({ name }).select("account");
+  if (!getSpAccount) {
+    return console.log("the servicepoint does not exist");
+  } else {
+    return getSpAccount.account;
+  }
+};
+
+const CreateSession = async (req, res) => {
+  const { start, end, name, phone } = req.body;
+
+  const servicepointAccountId = await GetSpByName(name);
+  const managerCouncerned = await User.findOne({ phone });
+
+  if (!servicepointAccountId) {
+    return res.status(404).json({ message: "the servicepoint does not exist" });
+  } else if (!managerCouncerned) {
+    return res.status(404).json({ message: "this manager does not exist" });
+  } else {
+    const servicepointCouncerned = await Spaccount.findById(
+      servicepointAccountId
+    );
+    //return res.status(200).json(servicepointCouncerned);
+    const session = new Session({
+     start,
+     end,
+     servicepoint: servicepointCouncerned._id,
+     manager: managerCouncerned._id,
+   });
+   try {
+     const data = await session.save();
+     servicepointCouncerned.sessions.push(data);
+     servicepointCouncerned.save();
+     managerCouncerned.sessions.push(data);
+     managerCouncerned.save();
+     if (!data) {
+       return res.status(404).json({ message: "session is not created" });
+     } else {
+       return res.status(201).json(data);
+     }
+   } catch (error) {
+     return res.status(404).json({ message: "session is not created" });
+   }
+  }
+};
+
+module.exports = {
+  CreateSession,
+};
--- routes/sessions.js
+++ routes/sessions.js
@@ -4,31 +4,10 @@ const router = express.Router();
 const { Session } = require("../models/sessions");
 const { User } = require("../models/user");
 const { Spaccount } = require("../models/spaccount");
+const {CreateSession} = require("../controller/session")
 
 //create session
-router.post("/", async (req, res) => {
-  const { start, end, name, phone } = req.body;
-
-  const servicepointCouncerned = await Spaccount.findOne({ name });
-  const managerCouncerned = await User.findOne({ phone });
-
-  const session = new Session({
-    start,
-    end,
-    servicepoint: servicepointCouncerned._id,
-    manager: managerCouncerned._id,
-  });
-  const data = await session.save();
-  servicepointCouncerned.sessions.push(data);
-  servicepointCouncerned.save();
-  managerCouncerned.sessions.push(data);
-  managerCouncerned.save();
-  if (!data) {
-    return res.status(404).json({ message: "session is not created" });
-  } else {
-    return res.status(201).json(data);
-  }
-});
+router.post("/", CreateSession);
 
 //get sessions
 router.get("/", async (req, res) => {
@@ -56,28 +35,28 @@ router.put("/:id", async (req, res) => {
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
   const accountData = await Session.findById(id)
-  .populate({
-    path: "transactions",
-    populate: {
-      path: "receiver",
-      select: { owner: 1 },
+    .populate({
+      path: "transactions",
       populate: {
-        path: "owner",
-        select: { phone: 1 },
+        path: "receiver",
+        select: { owner: 1 },
+        populate: {
+          path: "owner",
+          select: { phone: 1 },
+        },
       },
-    },
-  })
-  .populate({
-    path: "transactions",
-    populate: {
-      path: "sender",
-      select: { owner: 1 },
+    })
+    .populate({
+      path: "transactions",
       populate: {
-        path: "owner",
-        select: { phone: 1 },
+        path: "sender",
+        select: { owner: 1 },
+        populate: {
+          path: "owner",
+          select: { phone: 1 },
+        },
       },
-    },
-  });
+    });
   res.send(accountData);
 });
 
